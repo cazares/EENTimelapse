77dfe4355b60eabb01bf1f8a9ebed08e1eea902d
cazares <miguel.cazares89@gmail.com>
2014-05-29 16:18:45 -0500

    Initial Commit

---
+++ EENTimelapse/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  EENTimelapse
+//
+//  Created by Miguel Cazares on 5/29/14.
+//  Copyright (c) 2014 EagleEye Networks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
